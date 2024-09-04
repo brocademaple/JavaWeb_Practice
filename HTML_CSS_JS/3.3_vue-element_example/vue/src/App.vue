70d033ba9c8e783efbed940a71a2b8394297a4a4
mmm <12743738+ma-ma-ma-ma@user.noreply.gitee.com>
2024-09-04 10:30:46 +0800

    第二次上机 用bootstrap做网页
    第二次上机 用bootstrap做网页

---
+++ HTML_CSS_JS/3.3_vue-element_example/vue/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <!-- {{message}} -->
+    <element-view></element-view>
+  </div>
+</template>
+
+<script>
+import ElementView from './views/ElementView.vue'
+export default {
+  components: { ElementView },
+  data(){
+    return {
+      "message":"hello world"
+    }
+  }
+}
+</script>
+<style>
+
+</style>
